70eedbc44a52e3a92d63a000f50d6915717df5d6
PFLA <maxfritzsch100@gmail.com>
2024-10-25 19:52:26 +0200

    added copypasta command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use std::{fs::File, io::Read};
-use serenity::{all::{ Message, Reaction, Ready}, async_trait, prelude::*};
+use rand::{Rng};
+use serde::Deserialize;
+use serenity::{all::{ ChannelId, Member, Message, Reaction, Ready}, async_trait, prelude::*};
 
+#[derive(Deserialize)]
+struct Data{
+    verified_message_id: u64,
+    verified_emoji: String,
+    verified_role_id: u64,
+    welcome_channel_id: u64,
+    copypasta: Vec<String>,
+}
 
+struct Handler{
+    data:Data,
+}
 
-struct Handler;
+impl Handler {
+    fn new(data: Data) -> Self{
+        Handler {data}
+    }
+}
 #[async_trait]
 impl EventHandler for Handler{  
     //message commands: key symbol = >
@@ -18,6 +35,8 @@ impl EventHandler for Handler{
                     let res = if args==""{"no arguments found"} else {args};
                     msg.channel_id.say(ctx.http, res)
                 },
+                //"storage" => msg.channel_id.say(ctx.http, content),
+                "copypasta" => msg.channel_id.say(ctx.http,self.data.copypasta[rand::thread_rng().gen_range(0..self.data.copypasta.len())].as_str()),
                 _ => msg.channel_id.say(ctx.http,"unknown command"),
             };
 
@@ -27,13 +46,17 @@ impl EventHandler for Handler{
         }
     }
 
+    async fn guild_member_addition(&self, ctx:Context, mem:Member){
+        ChannelId::new(self.data.welcome_channel_id).say(ctx.http, format!("welcome {} to Quantum!",mem.nick.unwrap_or("".to_string()))).await.unwrap();
+    }
+
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
 
     async fn reaction_add (&self, ctx:Context, rct:Reaction) {
-       if rct.message_id == 1299380119848353924 && rct.emoji.unicode_eq("✅"){
-        rct.member.unwrap().add_role(ctx.http, 1299385080061366282).await.unwrap();
+       if rct.message_id == self.data.verified_message_id && rct.emoji.unicode_eq(&self.data.verified_emoji){
+        rct.member.unwrap().add_role(ctx.http, self.data.verified_role_id).await.unwrap();
        } 
     }
 }
@@ -42,12 +65,16 @@ impl EventHandler for Handler{
 async fn main() {
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MESSAGE_REACTIONS;
+        | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MESSAGE_REACTIONS | GatewayIntents::GUILD_MEMBERS;
 
     let mut token = String::new();
+    let mut data_string = String::new();
     File::open("token.txt").expect("no token file").read_to_string(&mut token).expect("file read failed");
+    File::open("data.toml").expect("no data toml file").read_to_string(&mut data_string).expect("file read failed");
 
-    let mut client = Client::builder(token, intents).event_handler(Handler).await.expect("building client failed");
+    let data:Data = toml::from_str(&data_string).expect("deserilazation of data failed");
+
+    let mut client = Client::builder(token, intents).event_handler(Handler::new(data)).await.expect("building client failed");
 
     if let Err(e) = client.start().await {
         println!("reason for Error:{e}");
