a3cb4019b9c10077bae68e4b90bb5b32aac3db46
PFLA <maxfritzsch100@gmail.com>
2024-10-25 17:46:30 +0200

    prepared command infrastructure and added echo command

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,24 @@ use serenity::{all::{ Message, Reaction, Ready}, async_trait, prelude::*};
 struct Handler;
 #[async_trait]
 impl EventHandler for Handler{  
+    //message commands: key symbol = >
     async fn message(&self, ctx:Context, msg:Message){
-        if msg.content == "!nono"{
-            msg.channel_id.say(ctx.http, "raaaaaaaa").await.unwrap();             
+        if msg.content.starts_with(">"){
+            let cmd = &msg.content[1..msg.content.find(" ").unwrap_or_else(||msg.content.len())];
+            let args = &msg.content[msg.content.find(" ").unwrap_or_else(||cmd.len()+1)..msg.content.len()];
+            
+
+            let execute = match cmd{
+                "echo" => {
+                    let res = if args==""{"no arguments found"} else {args};
+                    msg.channel_id.say(ctx.http, res)
+                },
+                _ => msg.channel_id.say(ctx.http,"unknown command"),
+            };
+
+            execute.await.unwrap();
+        
+        
         }
     }
 
